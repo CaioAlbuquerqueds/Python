6739c3a1ecf71898d6fb61812bd9a0c15e7090bb
Rogério Alcantara <rogerio.alunos@cfp127aluno.local>
2023-09-26 20:08:28 -0300

    IMC_Python

---
+++ teste.py
@@ -0,0 +1,5 @@
+altura = float(input('Coloque a sua altura: '))
+peso = float(input('Coloque o seu peso: '))
+imc = peso / (altura**2)
+
+print(f"Com seu peso de {peso}Kgs e com a sua altura de {altura}m, o seu IMC é:{imc}")
